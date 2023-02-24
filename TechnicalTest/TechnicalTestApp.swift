be4aee67f0c6474125ba70eec65593aa32684a50
Nolot Mathieu <mathieu-n@outlook.fr>
2023-02-24 13:46:56 +0100

    Initial Commit

---
+++ TechnicalTest/TechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalTestApp.swift
+//  TechnicalTest
+//
+//  Created by Mathieu Nolot on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
